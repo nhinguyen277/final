53df221bb4b8e1c953fe734c34bdbb758304ec1f
hatha <144495047+nhinguyen277@users.noreply.github.com>
2025-04-10 19:26:09 -0400

    second commit

--- tests/testcases.spec.js
+++ tests/testcases.spec.js
@@ -34,3 +34,32 @@ test("Login using locked_out_user", async ({ page }) => {
     "Epic sadface: Sorry, this user has been locked out."
   );
 });
+
+test("View a product using standard user", async ({ page }) => {
+  console.log("Navigating to website...");
+  await page.goto("https://www.saucedemo.com/");
+  console.log("Fill username with standard_user");
+  await page.fill("#user-name", "standard_user");
+  console.log("Fill password");
+  await page.fill('input[type="password"]', "secret_sauce");
+  console.log("Click submit");
+  await page.click('input[type="submit"]');
+
+  console.log("Successfully login. Navigate to inventory page");
+  await page
+    .waitForURL("**/inventory.html", { timeout: 10000 })
+    .catch(() => {});
+
+  console.log("Click a title of Sauce Labs Backpack to view");
+  await page.click(".inventory_item_name");
+
+  console.log("View product information");
+  await page
+    .waitForURL("**/inventory-item.html?id=4", { timeout: 10000 })
+    .catch(() => {});
+
+  const productHeader = await page.locator(
+    ".inventory_details_name.large_size"
+  );
+  await expect(productHeader).toContainText("Sauce Labs Backpack");
+});
